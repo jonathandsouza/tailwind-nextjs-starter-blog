9900830ef1100c979ce0240accfc3002a0cb0698
Jonathan Dsouza <mail2jona@yhoo.com>
2023-06-24 23:07:24 +0200

    added new article

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -42,10 +42,10 @@ const siteMetadata = {
 		// content security policy in the `next.config.js` file.
 		// Select a provider and use the environment variables associated to it
 		// https://vercel.com/docs/environment-variables
-		provider: 'giscus', // supported providers: giscus, utterances, disqus
+		provider: null, //'giscus', // supported providers: giscus, utterances, disqus
 		giscusConfig: {
 			// Visit the link below, and follow the steps in the 'configuration' section
-			// https://giscus.app/
+			// https://giscus.app/`
 			repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
 			repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
 			category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
